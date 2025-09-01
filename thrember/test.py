a7ba5f2d41c949b9c691c53fd1c11e944a3fcd4a
AFTERLiFE <77843063+afterlife0@users.noreply.github.com>
2025-09-01 17:40:10 +0530

    Update test.py

--- thrember/test.py
+++ thrember/test.py
@@ -1,3 +1,3 @@
 from download import download_dataset
 
-download_dataset(download_dir="A:/Collage/PROJECT/antivirus/ml2/data", split="all", file_type="all")
+download_dataset(download_dir="ember2024", split="all", file_type="all")
